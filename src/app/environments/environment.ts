7a0fa51a62a957c3914d90bff381c3cddbb487b0
ahmedyousry0098 <ahmedyousry098@gmail.com>
2023-09-26 20:38:42 +0300

    add movies service

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
-    BASE_URL: 'https://api.themoviedb.org',
+    BASE_URL: 'https://api.themoviedb.org/3',
     ACCESS_TOKEN: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNWVhOWYxZjg3ZjA0NzQ3MDU4MjE2Mzc5MmE0Y2MyMyIsInN1YiI6IjY1MTI4NzYwYTkxMTdmNzY1OWIwNDAwMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KgsRz-TMmi2h-7Eyr6X255dPu7m_Wn9bmM3tbTAzdNE",
     API_KEY: "35ea9f1f87f047470582163792a4cc23"
 }
--- src/app/movies/movies.service.ts
+++ src/app/movies/movies.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import {Observable} from 'rxjs'
+import { IMovie, IMovieResponse } from './interfaces/movie.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
 
-  constructor() { }
+  constructor(private _HttpClient: HttpClient) { }
+
+  fetchProductsPage(page: number = 1): Observable<IMovieResponse> {
+    return this._HttpClient.get<IMovieResponse>(`${environment.BASE_URL}/movie/popular`, {
+      params: {
+        page
+      }
+    })
+  }
+
+  fetchProductById(id: number): Observable<IMovie> {
+    return this._HttpClient.get<IMovie>(`${environment.BASE_URL}/movie/${id}`)
+  }
+
+  fetchRecommendedMovies(movieId: number): Observable<IMovieResponse> {
+    return this._HttpClient.get<IMovieResponse>(`${environment.BASE_URL}/movie/${movieId}/recommendations`)
+  }
+
+  searchMovie(movieName: string): Observable<IMovieResponse> {
+    return this._HttpClient.get<IMovieResponse>(`${environment.BASE_URL}/search/movie`, {
+      params: {
+        query: movieName
+      }
+    })
+  }
 }
