815463a9bed93a22f9e0ea5bbe655828e5bbed7b
Aya Othman <ayaaothman96@gmail.com>
2023-09-27 19:45:24 +0300

    adding movie rating UI

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -5,10 +5,17 @@ import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesListComponent } from './movies-list/movies-list.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbRating } from '@ng-bootstrap/ng-bootstrap';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @NgModule({
   declarations: [MoviesListComponent, MovieCardComponent],
-  imports: [CommonModule, MoviesRoutingModule, FontAwesomeModule, NgbRating],
+  imports: [
+    CommonModule,
+    MoviesRoutingModule,
+    FontAwesomeModule,
+    NgbRating,
+    NgbPaginationModule,
+  ],
 })
 export class MoviesModule {}
