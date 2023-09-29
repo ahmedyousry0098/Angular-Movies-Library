ce3887fe494dbea7025dbf7bf02a4ed8190a4671
Sherif-jr <94845939+Sherif-jr@users.noreply.github.com>
2023-09-30 01:28:58 +0300

    remove from favorites button works!

--- src/app/wish-list/wish-card/wish-card.component.ts
+++ src/app/wish-list/wish-card/wish-card.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { faHeart, faStar } from '@fortawesome/free-solid-svg-icons';
 import { faStar as faOutlineStar } from '@fortawesome/free-regular-svg-icons';
 import { IMovie } from 'src/app/movies/interfaces/movie.interface';
+import { MoviesService } from '../../movies/movies.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-wish-card',
@@ -14,7 +16,14 @@ export class WishCardComponent {
   heartIcon = faHeart;
   starIcon = faStar;
   outlinedStarIcon = faOutlineStar;
+  constructor(private moviesService: MoviesService, private router: Router) {}
   ngOnInit(): void {
     this.rate = Math.round(this.movie.vote_average / 2);
+  }
+
+  removeFromFavorite(id: number): void {
+    this.moviesService.favHandler(id, false).subscribe((response) => {
+      console.log(response);
+    });
   }
 }
